2d245bcdc4d0ab27f87f9101658a3152213b83cc
andrewsayegh <andrew_sayegh@student.wayland.k12.ma.us>
2017-11-27 09:14:45 -0500

    Added some methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.*;
+
 /**
  * Created by student on 11/27/17.
  */
 public class Main {
+
+    public int length(Object[] obj){
+        int length = 0;
+        for (Object i: obj) {
+            length++;
+        }
+        return length;
+    }
+
+    public Object clone(Object obj){
+        File file = new File("/scripts/Elements.txt");
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return obj;
+    }
+
 }
